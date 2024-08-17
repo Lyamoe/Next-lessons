18b020b8d0121d6afde110bfa858e33bad530a51
Lyamoe <karinesanper@gmail.com>
2024-08-17 12:20:36 -0300

    Create files

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("lessons_data");
+const tableBody = document
+    .getElementById("data")
+    .getElementsByTagName("tbody")[0];
+
+// const newRow = document.createElement("tr");
+// const subjectCell = document.createElement("td");
+// const activityCell = document.createElement("td");
+// const deadlineCell = document.createElement("td");
+
+// module.exports = { sum, subtract };
---
+++ script.test.js
@@ -0,0 +1,5 @@
+// const { sum, subtract } = require('./script'); 
+
+// test('sums two numbers', () => {
+//   expect(sum(2, 3)).toBe(5);
+// });
